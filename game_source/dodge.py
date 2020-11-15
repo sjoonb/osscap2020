edbda021a137493f599556445ca02ffab617c5aa
sjoonb <sungjun9808@gmail.com>
2020-11-15 17:38:39 +0900

    added keyboard mode to dodge.py

--- game_source/dodge.py
+++ game_source/dodge.py
@@ -7,27 +7,54 @@
 import pygame, random, sys, time, pygcurse
 from pygame.locals import *
 
+import LED_display as TLD
+import random
+import threading
+import keyboard
+import time
+import os
+import copy
+
 GREEN = (0, 255, 0)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 RED = (255,0,0)
 
-WINWIDTH = 40
-WINHEIGHT = 50
+WINWIDTH = 32
+WINHEIGHT = 16
 TEXTCOLOR = WHITE
 BACKGROUNDCOLOR = (0, 0, 0)
-FPS = 40
+FPS = 20
 BADDIEMINSIZE = 1
 BADDIEMAXSIZE = 5
 BADDIEMINSPEED = 4
 BADDIEMAXSPEED = 1
-ADDNEWBADDIERATE = 3
+ADDNEWBADDIERATE = 50
 
+# Make board
 win = pygcurse.PygcurseWindow(WINWIDTH, WINHEIGHT, fullscreen=False)
 pygame.display.set_caption('Pygcurse Dodger')
 win.autoupdate = False
 
+# Modified to play in LED Matrix
+
+t=threading.Thread(target=TLD.main, args=())
+t.setDaemon(True)
+t.start()
+
+# Screen
+iScreen = [[0 for i in range(WINWIDTH)] for j in range(WINHEIGHT)]
+
+# Mode
+mode_list = ['mouse', 'keyboard', 'sensor']
+mode = mode_list[1]
+
+moveLeft = False
+moveRight = False
+counter = 0
+
 def main():
+    global moveLeft, moveRight, counter
     showStartScreen()
     pygame.mouse.set_visible(False)
     mainClock = pygame.time.Clock()
@@ -37,11 +64,15 @@ def main():
     while True:
         if gameOver and time.time() - 4 > gameOverTime:
             newGame = True
+        
+        # First setting
         if newGame:
             newGame = False
-            pygame.mouse.set_pos(win.centerx * win.cellwidth, (win.bottom - 4) * win.cellheight)
+            
+            # Set first position of character
+            pygame.mouse.set_pos(win.centerx * win.cellwidth, (win.bottom) * win.cellheight)
             mousex, mousey = pygame.mouse.get_pos()
-            cellx, celly = win.getcoordinatesatpixel(mousex, mousey)
+            cellx, celly = WINWIDTH//2, WINHEIGHT-1
             baddies = []
             baddieAddCounter = 0
             gameOver = False
@@ -52,11 +83,40 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 terminate()
-            if event.type == MOUSEMOTION and not gameOver:
-                mousex, mousey = event.pos
-                cellx, celly = win.getcoordinatesatpixel(mousex, mousey)
+        # Character's move
+
+            # mouse mode
+            if mode == 'mouse':
+                if event.type == MOUSEMOTION and not gameOver:
+                    mousex, mousey = event.pos
+                    cellx, celly = win.getcoordinatesatpixel(mousex, mousey)
+
+            # keyboard mode
+            elif mode == 'keyboard':
+                if event.type == KEYDOWN:
+                    if event.key == K_LEFT or event.key == ord('a'):
+                        if not gameOver:
+                            cellx -= 1
+                            counter += 1
+                        moveRight = False
+                        moveLeft = True
+                    if event.key == K_RIGHT or event.key == ord('d'):
+                        if not gameOver:
+                            cellx += 1
+                            counter += 1
+                        moveLeft == False
+                        moveRight = True
+                if event.type == KEYUP:
+                    if event.key == K_LEFT or event.key == ord('a'):
+                        counter = 0
+                        moveLeft = False
+                    if event.key == K_RIGHT or event.key == ord('d'):
+                        counter = 0
+                        moveRight = False 
+        
 
         # add new baddies if needed
+        # ADDNEWBADDIERATE determines amount of new baddies
         if baddieAddCounter == ADDNEWBADDIERATE:
             speed = random.randint(BADDIEMAXSPEED, BADDIEMINSPEED)
             baddies.append({'size': random.randint(BADDIEMINSIZE, BADDIEMAXSIZE),
@@ -76,9 +136,12 @@ def main():
                 baddies[i]['movecounter'] = baddies[i]['speed']
             else:
                 baddies[i]['movecounter'] -= 1
-
+    
+            # print(i, baddies[i]['y'])
+            
             if baddies[i]['y'] > win.height:
                 del baddies[i]
+            
 
 
         # check if hit
@@ -90,19 +153,45 @@ def main():
                     break
             score += 1
 
-        # draw baddies to screen
+        # draw screen
+        oScreen = copy.deepcopy(iScreen)
+
+        # draw baddies to screen (Mouse Mode)
         for baddie in baddies:
             win.fill('#', GREEN, BLACK, (baddie['x'], baddie['y'], baddie['size'], baddie['size']))
+            fillMatrix(baddie['x'], baddie['y'], baddie['size'], oScreen, WINWIDTH, WINHEIGHT)
+            #for i in oScreen:
+            #    print(i)
 
+        # draw character to screen
         if not gameOver:
             playercolor = WHITE
+            oScreen[celly][cellx] = 2
         else:
             playercolor = RED
+            oScreen[celly][cellx] = 3
             win.putchars('GAME OVER', win.centerx-4, win.centery, fgcolor=RED, bgcolor=BLACK)
+
+        if mode == 'keyboard':
+            # todo - Boost
+            if not gameOver:
+                if counter > 3:
+                    if moveRight: cellx += 1
+                    if moveLeft: cellx -= 1
+                elif moveRight or moveLeft:
+                    counter += 1
+
+                if cellx < 0:
+                    cellx = 0
+                elif cellx > WINWIDTH -1:
+                    cellx = WINWIDTH-1
 
         win.putchar('@', cellx, celly, playercolor)
         win.putchars('Score: %s' % (score), win.width - 14, 1, fgcolor=WHITE)
         win.update()
+        
+
+        drawMatrix(oScreen)
         mainClock.tick(FPS)
 
 
@@ -126,6 +215,35 @@ def checkForKeyPress():
         return event.key
     return None
 
+# Game display
+
+def fillMatrix(bx, by, size, screen, w, h):
+    for i in range(by, by+size):
+        for j in range(bx, bx+size):
+            if i < 0 or i >= h or j < 0 or j >= w:
+                continue
+            #print(i, j)
+            screen[i][j] = 1  
+
+def fillCharacter(cx, cy, screen): 
+    if i < 0 or i >= h or j < 0 or j >= w:
+        return
+
+def drawMatrix(array):
+    for x in range(len(array[0])):
+        for y in range(len(array)):
+            if array[y][x] == 0:
+                TLD.set_pixel(x, y, 0)
+            elif array[y][x] == 1:
+                TLD.set_pixel(x, y, 2)
+            elif array[y][x] == 2:
+                TLD.set_pixel(x, y, 7)
+            elif array[y][x] == 3:
+                TLD.set_pixel(x, y, 1)
+            else:
+                continue
+
+# Game mode
 
 def terminate():
     pygame.quit()
