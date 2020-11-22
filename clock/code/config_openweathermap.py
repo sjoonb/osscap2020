c7e371e1d8202d89a9a14ea9779e516d95d96b02
sh <70614061+suh9023@users.noreply.github.com>
2020-11-23 03:36:17 +0900

    delete

--- clock/code/config_openweathermap.py
+++
@@ -1,9 +0,0 @@
-# -----------------------------------------------------------------------------
-# OpenWeatherMap details
-# -----------------------------------------------------------------------------
-CityLocation = 2639964  # Potton
-WeatherUnits = 'imperial'
-WeatherURLFormat = 'http://api.openweathermap.org/data/2.5/forecast?id={}&APPID={}'
-OpenWeatherMapKey = 'XXXXXX'
-SecondsBetweenWeatherRefresh = 3600
-SecondsBetweemWeatherRefreshOnError = 60
