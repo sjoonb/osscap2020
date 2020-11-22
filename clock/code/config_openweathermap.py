8947ebf8f7ab4675086073a894ade52aafc71462
Suhyeon Lee <suh9023@gmail.com>
2020-11-23 03:16:27 +0900

    clock added by Suhyeon

---
+++ clock/code/config_openweathermap.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+# OpenWeatherMap details
+# -----------------------------------------------------------------------------
+CityLocation = 2639964  # Potton
+WeatherUnits = 'imperial'
+WeatherURLFormat = 'http://api.openweathermap.org/data/2.5/forecast?id={}&APPID={}'
+OpenWeatherMapKey = 'XXXXXX'
+SecondsBetweenWeatherRefresh = 3600
+SecondsBetweemWeatherRefreshOnError = 60
