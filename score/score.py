580de3894c268046a48a02ea10a1795bb11f33a8
predis3299 <predis7454@gmail.com>
2020-11-25 19:01:04 +0900

    score modified by Jihun

--- score/score.py
+++ score/score.py
@@ -1,12 +1,26 @@
 import os
 
-def add_score_dodger(name, score):
+def add_score_dodger(score):
+    while True:
+        name = input("Enter your name : ")
+        if (name.isalpha() == True) and (name.isupper() == True) and (len(name) == 3):
+            break
+        else:
+            print("Wrong input!")
+            continue
     f = open("score_dodger.txt", 'a')
     data = str(name) + ' ' + str(score) + '\n'
     f.write(data)
     f.close()
 
-def add_score_bb(name, score):
+def add_score_bb(score):
+    while True:
+        name = input("Enter your name : ")
+        if (name.isalpha() == True) and (name.isupper() == True) and (len(name) == 3):
+            break
+        else:
+            print("Wrong input!")
+            continue
     f = open("score_bb.txt", 'a')
     data = str(name) + ' ' + str(score) + '\n'
     f.write(data)
@@ -31,13 +45,13 @@ def print_score_bb():
     for line in lines:
         tmp = line.split()
         score_dict[tmp[0]] = int(tmp[1])
-    s_score = sorted(score_dict.items(), key = lambda x:x[1], reverse = True)
+    s_score = sorted(score_dict.items(), key = lambda x:x[1])
     for i in range(3):
         print("{} : {}".format(s_score[i][0], s_score[i][1]))
     f.close()
 
-def clear_score():
+def clear_score_dodger():
     os.remove("score_dodger.txt")
 
-def clear_score():
+def clear_score_bb():
     os.remove("score_bb.txt")
